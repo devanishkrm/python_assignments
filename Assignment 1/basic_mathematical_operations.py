6570985bcf771bb7591db9f65712d94b09dc3c87
Anish  MM <anishmm@OM-Anish.local>
2025-12-14 23:37:22 +0530

    assignment 1

---
+++ Assignment 1/basic_mathematical_operations.py
@@ -0,0 +1,22 @@
+#
+# Problem Statement: Write a Python program that does the following:
+# 1.  Takes two numbers as input from the user.
+# 2.  Performs the basic mathematical operations on these two numbers:
+# Addition
+# Subtraction
+# Multiplication
+# Division
+#
+
+num1 = float(input("Enter the fist number:"))
+num2 = float(input("Enter the second number:"))
+
+add = num1 + num2
+sub = num1 - num2
+div = num1 / num2
+mul = num1 * num2
+
+print("Addition:", add)
+print("Subtraction:", sub)
+print("Multiplication:", mul)
+print("Division:", div)
